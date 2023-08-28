27d036b6c4afcdd7ba72c9d104c7a997ec2a5a4a
Oneleaon <devike975@gmail.com>
2023-08-28 15:31:01 -0400

    Adds other Card types extending Card. Adds a class to test fetching the information from the json file and putting them into an Array of Cards

--- Card/card-tester.js
+++ Card/card-tester.js
@@ -1,5 +1,28 @@
-include(Card);
+require('./card.js');
 
 async function populate() {
-    const requestURL = "https://github.com/Oneleaon/card-bot/raw/main/ygo-cards.json";
+    const requestURL = "https://raw.githubusercontent.com/Oneleaon/card-bot/card-implementation/ygo-cards.json";
+    const request = new Request(requestURL);
+
+    const response = await fetch(request);
+    return await response.json();
 }
+
+function convert(dataArray) {
+    const cards = [];
+    for (const card of dataArray) {
+        if (card.frameType = "spell")
+            cards.push(new Card(card.name, card.frameType, card.race, card.desc));
+    }
+    return cards;
+}
+
+async function main() {
+    const cardsRaw = await populate();
+    const cards = convert(cardsRaw.data);
+
+    console.log(cards[0].toString());
+    console.log(cards.length);
+}
+
+main();
--- Card/card.js
+++ Card/card.js
@@ -2,28 +2,65 @@ class Card {
     // CARD PROPERTIES
     #name = "";
     #frameType= "";
-    #desc = "";
     #race = "";
-
-    // Monster card exclusive properties
-    #attribute = "";
-    #atk = short;
-
-    // Link doesn't have
-    #def = short;
-    #level = short;
-
-    #linkval = short; // Link exclusive
+    #desc = "";
 
     // CONSTRUCTORS
     // Spell/Trap
     constructor(name, frameType, race, desc) { this.#name = name; this.#frameType = frameType; this.#race = race; this.#desc = desc; }
 
-    // Monster
-    constructor(name, frameType, race, desc, attribute, atk, def, level) { this.#name = name; this.#frameType = frameType; this.#race = race; this.#desc = desc; this.#attribute = attribute; this.#atk = atk; this.#def = def; this.#level = level; }
-
     // Link monster
     constructor(name, frameType, race, desc, attribute, atk, linkval) { this.#name = name; this.#frameType = frameType; this.#race = race; this.#desc = desc; this.#attribute = attribute; this.#atk = atk; this.#linkval = linkval; }
 
+    // METHODS
+    get name() { return this.#name; }
+    get frame() { return this.#frameType; }
+    get race() { return this.#race; }
+    get desc() { return this.#desc; }
 
+    toString() {
+        return `${this.#name}\n${this.#frameType}\n${this.#race}\n${this.#desc}`;
+    }
+}
+
+class SpellCard extends Card {
+    super();
+}
+
+class TrapCard extends Card {
+    super()
+}
+
+class MonsterCard extends Card {
+    #attribute = "";
+    #atk = short;
+    #def = short;
+    #level = short;
+
+    constructor(attribute, atk, def, level) {
+        super()
+        this.#attribute = attribute; this.#atk = atk; this.#def = def; this.#level = level;
+    }
+
+    get attribute() { return this.#attribute; }
+    get atk() { return this.#atk; }
+    get def() { return this.#def; }
+    get level() { return this.#level; }
+}
+
+class LinkMonsterCard extends Card {
+    #attribute = "";
+    #atk = short;
+    #linkval = short;
+    #linkmarkers = [];
+
+    constructor(attribute, atk, linkval, linkmarkers) {
+        super()
+        this.#attribute = attribute; this.#atk = atk; this.#linkval = linkval; this.#linkmarkers = levlinkmarkersel;
+    }
+
+    get attribute() { return this.#attribute; }
+    get atk() { return this.#atk; }
+    get linkval() { return this.#linkval; }
+    get linkmarkers() { return this.#linkmarkers; }
 }
