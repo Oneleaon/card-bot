d0f56302b97e3b57bcabba77d263a380a73b9a58
Oneleaon <devike975@gmail.com>
2023-08-28 13:25:13 -0400

    Adds a Card class with constructors for different types of cards

---
+++ Card/card.js
@@ -0,0 +1,29 @@
+class Card {
+    // CARD PROPERTIES
+    name = "";
+    frameType= "";
+    desc = "";
+    race = "";
+
+    // Monster card exclusive properties
+    attribute = "";
+    atk = short;
+
+    // Link doesn't have
+    def = short;
+    level = short;
+
+    linkval = short; // Link exclusive
+
+    // CONSTRUCTORS
+    // Spell/Trap
+    constructor(name, frameType, race, desc) { this.name = name; this.frameType = frameType; this.race = race; this.desc = desc; }
+
+    // Monster
+    constructor(name, frameType, race, desc, attribute, atk, def, level) { this.name = name; this.frameType = frameType; this.race = race; this.desc = desc; this.attribute = attribute; this.atk = atk; this.def = def; this.level = level; }
+
+    // Link monster
+    constructor(name, frameType, race, desc, attribute, atk, linkval) { this.name = name; this.frameType = frameType; this.race = race; this.desc = desc; this.attribute = attribute; this.atk = atk; this.linkval = linkval; }
+
+
+}
