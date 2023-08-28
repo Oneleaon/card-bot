c604debaff85004f844fcf776a4aed1a21530af6
Oneleaon <devike975@gmail.com>
2023-08-28 13:58:45 -0400

    Change Card properties to private

--- Card/card.js
+++ Card/card.js
@@ -1,29 +1,29 @@
 class Card {
     // CARD PROPERTIES
-    name = "";
-    frameType= "";
-    desc = "";
-    race = "";
+    #name = "";
+    #frameType= "";
+    #desc = "";
+    #race = "";
 
     // Monster card exclusive properties
-    attribute = "";
-    atk = short;
+    #attribute = "";
+    #atk = short;
 
     // Link doesn't have
-    def = short;
-    level = short;
+    #def = short;
+    #level = short;
 
-    linkval = short; // Link exclusive
+    #linkval = short; // Link exclusive
 
     // CONSTRUCTORS
     // Spell/Trap
-    constructor(name, frameType, race, desc) { this.name = name; this.frameType = frameType; this.race = race; this.desc = desc; }
+    constructor(name, frameType, race, desc) { this.#name = name; this.#frameType = frameType; this.#race = race; this.#desc = desc; }
 
     // Monster
-    constructor(name, frameType, race, desc, attribute, atk, def, level) { this.name = name; this.frameType = frameType; this.race = race; this.desc = desc; this.attribute = attribute; this.atk = atk; this.def = def; this.level = level; }
+    constructor(name, frameType, race, desc, attribute, atk, def, level) { this.#name = name; this.#frameType = frameType; this.#race = race; this.#desc = desc; this.#attribute = attribute; this.#atk = atk; this.#def = def; this.#level = level; }
 
     // Link monster
-    constructor(name, frameType, race, desc, attribute, atk, linkval) { this.name = name; this.frameType = frameType; this.race = race; this.desc = desc; this.attribute = attribute; this.atk = atk; this.linkval = linkval; }
+    constructor(name, frameType, race, desc, attribute, atk, linkval) { this.#name = name; this.#frameType = frameType; this.#race = race; this.#desc = desc; this.#attribute = attribute; this.#atk = atk; this.#linkval = linkval; }
 
 
 }
